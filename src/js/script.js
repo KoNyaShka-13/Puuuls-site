4a04d6808fa6d573fbcbd4adec892444d94e3fce
Alex <97723655+KoNyaShka-13@users.noreply.github.com>
2022-05-27 10:53:06 +0300

    пример данного слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,15 @@
-$(document).ready(function(){
-	$('.carousel__inner').slick({
-		peed: 1200,
-		adaptiveHeight: true,
-/*		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>' Данный путь будет работать только на сервере, на html файле так работать не будет, так как JS работает напрямую на сайте*/
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-		responsive: [
-		{
-			breakpoint: 768,/*работает, как медиа, то есть в низ, все правила будут работать до данной ширины*/
-			settings: {/*Данная ширина - это переход от пк к планшетным и телефонным версиям, как раз на такой ширине не удобно уже пользоваться кнопками*/
-				dots: true,
-				arrows: false
-				}
-		}
-		]
-	});
+const slider = tns({/*Минус данного слайдера состоит в том, что кнопки находятся в контейнере и при адаптации будут возникать проблеы с ними, по этому, данный слайдер (tiny slider) используется только тогда, когда кнопки находятся снизу или сверху слайдера, то есть в простых местах, где проблем с ними не будет*/
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});/*Делаем так, чтобы картинка кнопки работала, как кнопка, за место тех, что создались при помощи tiny-slider*/
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
 });
