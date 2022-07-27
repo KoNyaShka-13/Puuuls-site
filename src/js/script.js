31af59b5b0f472d015d4ddbd66df37aa0de92044
Alex <97723655+KoNyaShka-13@users.noreply.github.com>
2022-07-27 11:06:52 +0300

    к

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 		speed: 1200,
 		adaptiveHeight: true,
 		center: true,
-/*		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>' Данный путь будет работать только на сервере, на html файле так работать не будет, так как JS работает напрямую на сайте*/
+//		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>' Данный путь будет работать только на сервере, на html файле так работать не будет, так как JS работает напрямую на сайте*/
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 		responsive: [
 			{
-			breakpoint: 991,/*работает, как медиа, то есть в низ, все правила будут работать до данной ширины*/
-			settings: {/* 768 - Данная ширина - это переход от пк к планшетным и телефонным версиям, как раз на такой ширине не удобно уже пользоваться кнопками*/
+			breakpoint: 991,//работает, как медиа, то есть в низ, все правила будут работать до данной ширины
+			settings: {// 768 - Данная ширина - это переход от пк к планшетным и телефонным версиям, как раз на такой ширине не удобно уже пользоваться кнопками
 				dots: true,
 				arrows: false,
 				}
@@ -42,14 +42,14 @@ $(document).ready(function(){
 		$('.overlay, #consultation').fadeIn('slow');
 	});
 	$('.modal__close').on('click', function(){
-		$('.overlay, #consultation, #thanks, #order').fadeOut('clow');
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
 	});
 
 	$('.button-mini').each(function(i){
 		$(this).on('click', function(){
 			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
 			$('.overlay, #order').fadeIn('slow');
-		});
+		})
 	});
 
 
@@ -83,20 +83,21 @@ $(document).ready(function(){
 	validateForms('#consultation form');
 	validateForms('#order form');
 
-	$('input[name=phone]').mask("+7 (999) 999-99-99");/*маски работают, если в форме не указан type*/
+	$('input[name=phone]').mask("+7 (999) 999-99-99");//маски работают, если в форме не указан type
 
-	$('form').submit(function(e){
-		e.preventDefault();/*Отменить стандартное поведение браузера*/
-
-		if (!$(this).valid()) {/*Нужен для того, чтобы при валидации нельзя было отправлять пустые запросы*/
+	$('form').submit(function(e) {
+		e.preventDefault();
+		//Отменить стандартное поведение браузера
+		if (!$(this).valid()) {
+		//Нужен для того, чтобы при валидации нельзя было отправлять пустые запросы
 			return;
 		}
-
 		$.ajax({
 			type: "POST",
 			url: "mailer/smart.php",
 			data: $(this).serialize()
-		}).done(function() {//После того, как условие выше будет выполнено, выполняется условие ниже
+		}).done(function() {
+		//После того, как условие выше будет выполнено, выполняется условие ниже
 			$(this).find("input").val("");
 			$('#consultation, #order').fadeOut();
 			$('.overlay, #thanks').fadeIn('slow');
