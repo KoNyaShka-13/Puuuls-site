7cac4a8cdf4d287d2ab047b33e5a03063b95b41b
Alex <97723655+KoNyaShka-13@users.noreply.github.com>
2022-07-29 08:23:17 +0300

    Недочеты

--- src/js/script.js
+++ src/js/script.js
@@ -115,7 +115,7 @@ $(document).ready(function(){
 		}
 	});
 
-	$("a[href=#up]").click(function(){
+	$("a[href=#up]").click(function(){// В квадратных скорбках указывается атрибут
 		const _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
